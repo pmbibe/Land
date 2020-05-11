0770cb2bac4b59de3a875d49f53c4fe8a6ee0950
pmbibe <53257014+pmbibe@users.noreply.github.com>
2020-05-11 15:18:46 +0700

    Update landingpage.sh

--- landingpage.sh
+++ landingpage.sh
@@ -7,21 +7,20 @@ yum -y install yum-utils && \
 yum-config-manager --enable remi-php72 && \
 yum -y update && \
 yum -y install php && \
-yum -y install php-fpm php-gd php-json php-mbstring php-mysqlnd php-xml php-xmlrpc php-opcache >> /dev/null 2>&1
+yum -y install php-fpm php-gd php-json php-mbstring php-mysqlnd php-xml php-xmlrpc php-opcache >> /dev/null
 echo "Starting PHP"
-systemctl start php-fpm >> /dev/null 2>&1
+systemctl start php-fpm >> /dev/null
 echo "PHP started"
 echo "Installing Redis"
-sudo yum install redis -y >> /dev/null 2>&1
+sudo yum install redis -y >> /dev/null
 sudo systemctl enable redis
 echo "Starting redis"
 sudo systemctl start redis.service
 echo "Redis started"
 echo "Installing Composer"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" >> /dev/null 2>&1
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer >> /dev/null 2>&1
-php –r “unlink(‘composer-setup.php’);” >> /dev/null 2>&1
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" >> /dev/null
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer >> /dev/null
 echo "Composer is installed"
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash - >> /dev/null 2>&1
-sudo yum install nodejs -y >> /dev/null 2>&1
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash - >> /dev/null
+sudo yum install nodejs -y >> /dev/null
 mkdir -p /srv/code_folder/public
